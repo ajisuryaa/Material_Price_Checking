951304241d4f3b372f9824620292aa998d8886a2
aji <rosevelt201@gmail.com>
2021-11-11 14:34:54 +0700

    - Add function Add New Material

--- app/src/main/java/com/putrabatam/materialstore/controller/Material.java
+++ app/src/main/java/com/putrabatam/materialstore/controller/Material.java
@@ -5,34 +5,26 @@ import android.graphics.Bitmap;
 public class Material {
     public String id, name, photo_address;
     public Bitmap photo;
-    public int current_stock, kmin, kmax;
+    public String satuan;
+    public int price;
 
     public Material(){
 
     }
-    public Material(String id, String name, Bitmap photo, String kmin, String kmax){
-        this.id = id;
-        this.name = name;
-        this.photo = photo;
-        this.kmin = value_of_K(kmin);
-        this.kmax = value_of_K(kmax);
-    }
 
-    int value_of_K(String kmin){
-        if(kmin.equals("") || kmin.length() <= 0 || kmin.isEmpty()){
-            return 0;
-        } else {
-            return Integer.parseInt(kmin);
-        }
-    }
-
-    public void set_material(String id, String name, String photo, int current_stock, int kmin, int kmax){
+    public Material(String id, String name, String photo, int price, String satuan){
         this.id = id;
         this.name = name;
         this.photo_address = photo;
-        this.current_stock = current_stock;
-        this.kmin = kmin;
-        this.kmax = kmax;
+        this.price = price;
+        this.satuan = satuan;
+    }
+
+    public void set_material(String name, Bitmap photo, int price, String satuan){
+        this.name = name;
+        this.photo = photo;
+        this.price = price;
+        this.satuan = satuan;
     }
 
     public String validation_adding_material(){
@@ -40,11 +32,9 @@ public class Material {
             return "ID material tidak boleh kosong";
         } else if(name.equals("") || name.length()<=0 || name.isEmpty()){
             return "Nama material tidak boleh kosong";
-        } else if(kmin == 0){
+        } else if(price == 0){
             return "KMIN material tidak boleh kosong";
-        } else if(kmax == 0){
-            return "KMAX material tidak boleh kosong";
-        } else {
+        }  else {
             return "done";
         }
     }
--- app/src/main/java/com/putrabatam/materialstore/view/Form_Material.java
+++ app/src/main/java/com/putrabatam/materialstore/view/Form_Material.java
@@ -2,30 +2,68 @@ package com.putrabatam.materialstore.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
 import com.putrabatam.materialstore.R;
+import com.putrabatam.materialstore.controller.DataPart;
+import com.putrabatam.materialstore.controller.Material;
+import com.putrabatam.materialstore.utils.HttpsTrustManager;
+import com.putrabatam.materialstore.utils.ImageHandler;
+import com.putrabatam.materialstore.utils.PopUpMessage;
+import com.putrabatam.materialstore.utils.Server_Configuration;
+import com.putrabatam.materialstore.utils.VolleyMultipartRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Form_Material extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    RequestQueue requestQueue;
+    ProgressDialog progressDialog;
+    PopUpMessage popUpMessage = new PopUpMessage();
+
     public final int REQUEST_IMAGE_CAPTURE = 1;
     public final int REQUEST_IMAGE_GALLERY = 2;
 
+    EditText name, satuan, harga;
     Button simpan, pilih_foto;
+    ImageView material_photo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_material);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        progressDialog = new ProgressDialog(Form_Material.this);
+        requestQueue = Volley.newRequestQueue(Form_Material.this);
+        material_photo = findViewById(R.id.iv_material_photo_fm);
+        name = findViewById(R.id.et_name_fm);
+        satuan = findViewById(R.id.et_satuan_fm);
+        harga = findViewById(R.id.et_harga_fm);
         pilih_foto = findViewById(R.id.btn_choose_photo_fm);
         simpan = findViewById(R.id.btn_save_fm);
 
@@ -38,6 +76,87 @@ public class Form_Material extends AppCompatActivity implements PopupMenu.OnMenu
                 popup.show();
             }
         });
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable = (BitmapDrawable) material_photo.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                Material data_material = new Material();
+                data_material.set_material(
+                        name.getText().toString(),
+                        bitmap,
+                        Integer.valueOf(harga.getText().toString()),
+                        satuan.getText().toString()
+                );
+            }
+        });
+    }
+
+    //Fungsi untuk mengirim data material baru
+    private void Add_New_Material(final Material material) {
+        progressDialog.setMessage("Please Wait");
+        progressDialog.show();
+        progressDialog.setCancelable(false);
+        HttpsTrustManager.allowAllSSL();
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, Server_Configuration.address_add_new_material,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            if(obj.getBoolean("status")){
+                                Toast.makeText(Form_Material.this,
+                                        obj.getString("message"), Toast.LENGTH_LONG).show();
+                                Intent kembali = new Intent(Form_Material.this, Home_Admin.class);
+                                startActivity(kembali);
+                                finish();
+                            } else{
+                                Toast.makeText(Form_Material.this,
+                                        obj.getString("message"), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(Form_Material.this,
+                                    e.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", material.id);
+                params.put("satuan", material.satuan);
+                params.put("price", String.valueOf(material.price));
+                return params;
+            }
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("photo", new DataPart(imagename + ".jpg", ImageHandler.getFileDataFromDrawable(material.photo)));
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
     }
 
     @Override
